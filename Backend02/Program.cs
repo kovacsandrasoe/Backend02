ac196d8bc45eaea869d31ae9fa679eb0d19c700f
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-09-25 15:49:41 +0200

    xml demo

--- Backend02/Program.cs
+++ Backend02/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Backend02
+﻿using System.Xml.Linq;
+
+namespace Backend02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var xml = XDocument.Load("people.xml");
+            //foreach (var item in
+            //    xml.Element("people")!.Elements("person"))
+            //{
+            //    Console.WriteLine(item.Element("name")?.Value);
+            //}
+
+            //linq: 27 évnél idősebbek nevei
+            var q = from item in xml.Element("people")!.Elements("person")
+                    where int.Parse(item.Element("age")!.Value) > 27
+                    select item.Element("name")!.Value;
+
+            
         }
     }
 }
